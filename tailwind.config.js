7254d01175f9ab92ba9bfc62762f76a9da5535e0
Sapereza <s.perezanderson@gmail.com>
2022-08-05 14:41:54 -0400

    I have used color picker to get the proper background colors. I have spaced most elements out properly. I do not know how to place the radio buttons on the right of the text in the nav/shadow-inner does not seem to work to add a inner box shadow to the input field in the nav/nor does trying to make the search button have an offset appearence.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nav1: '#013298',
+        nav2: '#6898CE',
+        tomblurb1: '#FFCB99',
+        tomblurb: '#F27A2B'
+      }
+    },
   },
   plugins: [],
 }
